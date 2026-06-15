faeb64233f62e750ce5d79504902ad03f19c9466
SandoBall10 <sandovalballona@gmail.com>
2026-06-15 13:53:05 -0500

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/SandraFernandez/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
